ada26c01592ccce4372facf12737ad238b7de5bf
Mosqqqqq <1956794330@qq.com>
2024-01-19 17:50:44 +0800

    change middleware

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ import uvicorn
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-]
+# origins = [
+#     "http://localhost",
+#     "http://localhost:8080",
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
